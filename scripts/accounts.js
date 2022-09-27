13daba728ec5136a5f008edba0a31cbe2f8072b2
Remix Workspace <>
2022-09-27 16:16:48 +0530

    Event Example

---
+++ scripts/accounts.js
@@ -0,0 +1,17 @@
+/*
+    function main()
+    {
+
+    }
+    
+*/
+
+( async () => {
+
+    let accounts = await web3.eth.getAccounts();
+    console.log(accounts, accounts.length)
+    let balance = await web3.eth.getBalance(accounts[0])
+    console.log(balance)
+    let balanceInEth = web3.utils.fromWei(balance, "ether")
+    console.log(balanceInEth)
+})()
